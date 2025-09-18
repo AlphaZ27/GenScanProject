36cab691c44366e51e6ba794ed5ff4e910b598d7
AlphaZ27 <zmwarner30@gmail.com>
2025-09-18 09:47:30 -0400

    Minor Errors in Part 8 of GenScanProject App: -Separated the modules from :app and now its just firebase errors

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -11,29 +11,26 @@ android {
 
     defaultConfig {
         minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+//    buildTypes {
+//        release {
+//            isMinifyEnabled = false
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
+//        }
+//    }
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_17
+//        targetCompatibility = JavaVersion.VERSION_17
+//    }
 }
 
-kotlin {
-    jvmToolchain(17) // This is the modern way
-}
+//kotlin {
+//    jvmToolchain(17) // This is the modern way
+//}
 
 dependencies {
 
